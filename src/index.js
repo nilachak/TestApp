c5529e78f87656bb67c6c49a183cb78469c7ca58
nilachak <47153541+nilachak@users.noreply.github.com>
2021-09-09 18:36:36 +0530

    Updates done
    updates done

--- src/index.js
+++ src/index.js
@@ -12,3 +12,21 @@ app.get('/', function (req, res) {
 app.listen(3000, () => {
     console.log('Running on the port 3000...');
 });
+
+it(name, () => {
+        cy.wait(delayBetweenTests)
+        cy.visit(
+          `${baseUrl}?uid=${partyUid}&e=${email}&p=${program}`
+        )
+
+        cy.get('h1').should('exist')
+        cy.get('p')
+          .should('exist')
+          .and('have.length', 3)
+        cy.get('p').should('have.class', 'opt-ikea-intro')
+
+        cy.get('button')
+          .should('exist')
+          .click()
+
+      })
